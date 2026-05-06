9421846d94afdcf8791783ffddcdf255ceaf7db1
Davi Queiroz Santos <davi.santos@eval.digital>
2026-05-06 12:09:38 -0300

    Muitos codigos

---
+++ app/src/main/java/davi/com/github/cryptomonitor/service/MercadoBitcoinService.kt
@@ -0,0 +1,10 @@
+package davi.com.github.cryptomonitor.service
+
+import davi.com.github.cryptomonitor.model.TickerResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MercadoBitcoinService {
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker():Response<TickerResponse>
+}
